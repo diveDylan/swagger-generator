c451a3df9015699f021bbf2106eb8b28052b0058
liudongdong <1003054885@qq.com>
2020-11-13 10:30:48 +0800

    fetch code

--- lib/src/template/request.d.ts
+++ lib/src/template/request.d.ts
@@ -1,3 +1,4 @@
+import { SwaggerMethod } from '../../index.d';
 export declare const makeRequestCommentHeader: (summary: string, description: string) => string;
-export declare const makeRequestFunction: (url: string, functionName: string, method: "post" | "get" | "put" | "delete", requestTypes: string, responseTypes: string) => string;
-export declare const makePostNoReqParamsFunction: (url: string, functionName: string, method: "post" | "get" | "put" | "delete", responseTypes: string) => string;
+export declare const makeRequestFunction: (url: string, functionName: string, method: SwaggerMethod, requestTypes: string, responseTypes: string) => string;
+export declare const makePostNoReqParamsFunction: (url: string, functionName: string, method: SwaggerMethod, responseTypes: string) => string;
